b86b9b8164f5136e04002f2acd51437103979bf6
ManishkaX <anton.khudnitski@gmail.com>
2022-06-25 09:35:01 +0300

    empty

--- Serialization/Interfaces/ISerializer.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Serializer.Serialization.Interfaces
-{
-    public interface ISerializer
-    {
-        string Serialize(object obj);
-
-        object Deserialize(string str, Type type);
-    }
-}
